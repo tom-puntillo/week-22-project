b65ae58c799c3df02475d3e62a3712f4ed9c3a0b
tom-puntillo <tom.puntillo@gmail.com>
2023-08-14 04:09:35 +0000

    modified main.tf

--- main.tf
+++ main.tf
@@ -45,6 +45,6 @@ resource "aws_subnet" "private_subnet_2" {
   vpc_id     = aws_vpc.week_22_vpc.id
   cidr_block = var.private_sub_2
   tags = {
-    Name = "private_subnet_2"
+    Name = "public_subnet_2"
   }
 }
