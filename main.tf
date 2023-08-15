e0dec17aec4b9077644231a74cf2d787e31cfcdc
Thomas Puntillo <48040854+tom-puntillo@users.noreply.github.com>
2023-08-15 17:35:56 -0400

    added private rt and all rt associations (#8)

--- main.tf
+++ main.tf
@@ -83,3 +83,36 @@ resource "aws_route_table" "public_rt" {
     Name = "public_rt"
   }
 }
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.week_22_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.week_22_nat_gw.id
+  }
+
+  tags = {
+    Name = "private_rt"
+  }
+}
+
+resource "aws_route_table_association" "public_rt_association_a" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_rt_association_b" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "private_rt_association_a" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private_rt_association_b" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_rt.id
+}
