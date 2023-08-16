1729426b636873f1eae622ca6c9ee7d0651f13ff
tom-puntillo <tom.puntillo@gmail.com>
2023-08-16 00:43:14 +0000

    adjusted ec2 subnet id's

--- ec2_nginx_web_servers/main.tf
+++ ec2_nginx_web_servers/main.tf
@@ -2,12 +2,12 @@ resource "aws_instance" "web_server_1" {
   ami           = var.ami
   instance_type = var.instance_type
   user_data = var.user_data
-  subnet_id = "_2_tier_vpc.public_subnet_1_id"
+  subnet_id = module._2_tier_vpc.public_subnet_1_id
 }
 
 resource "aws_instance" "web_server_2" {
   ami           = var.ami
   instance_type = var.instance_type
   user_data = var.user_data
-  subnet_id = "_2_tier_vpc.public_subnet_2_id"
+  subnet_id = module._2_tier_vpc.public_subnet_2_id
 }
