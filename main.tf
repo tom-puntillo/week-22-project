8dc29b3ba9f3591da2957151ed612fd023c537fe
Thomas Puntillo <48040854+tom-puntillo@users.noreply.github.com>
2023-08-17 23:20:35 -0400

    corrected ec2 module and added security groups module (#16)

--- main.tf
+++ main.tf
@@ -4,7 +4,22 @@ module "vpc" {
   source = "./vpc"
 }
 
-module "ec2_web_servers" {
+module "ec2_web_server_1" {
   source = "./ec2"
+
+  subnets = module.vpc.public_subnet_1_id
+
+  security_groups = [module.security_groups.security_group_http, module.security_groups.security_group_tls]
 }
 
+module "ec2_web_server_2" {
+  source = "./ec2"
+
+  subnets = module.vpc.public_subnet_2_id
+
+  security_groups = [module.security_groups.security_group_http, module.security_groups.security_group_tls]
+}
+
+module "security_groups" {
+  source = "./security_groups"
+}
--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -2,10 +2,10 @@
 
 output "public_subnet_1_id" {
     description = "Public Subnet 1 ID"
-    value = aws_subnet.private_subnet_1.cidr_block
+    value = aws_subnet.public_subnet_1.cidr_block
 }
 
 output "public_subnet_2_id" {
     description = "Public Subnet 2 ID"
-    value = aws_subnet.private_subnet_2.cidr_block
+    value = aws_subnet.public_subnet_2.cidr_block
 }
