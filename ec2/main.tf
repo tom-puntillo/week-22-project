3dd81ab6618164dffc348af2327abe36ad1de5eb
Thomas Puntillo <48040854+tom-puntillo@users.noreply.github.com>
2023-08-18 17:43:21 -0400

    added the vpc input variable to the sg module (#19)

--- ec2/main.tf
+++ ec2/main.tf
@@ -3,5 +3,6 @@ resource "aws_instance" "web_server" {
   ami           = var.ami
   instance_type = var.instance_type
   user_data = var.user_data
+  security_groups = var.security_groups
 }
 
--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -2,12 +2,12 @@
 
 output "public_subnet_1_id" {
     description = "Public Subnet 1 ID"
-    value = aws_subnet.public_subnet_1.cidr_block
+    value = aws_subnet.public_subnet_1.id
 }
 
 output "public_subnet_2_id" {
     description = "Public Subnet 2 ID"
-    value = aws_subnet.public_subnet_2.cidr_block
+    value = aws_subnet.public_subnet_2.id
 }
 
 output "vpc_id" {
