21219414aec8091c3631a5d0ac7390791959f4f0
Thomas Puntillo <48040854+tom-puntillo@users.noreply.github.com>
2023-08-17 23:43:08 -0400

    corrected user data script and assisgned vpc id to sg (#17)

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -9,3 +9,8 @@ output "public_subnet_2_id" {
     description = "Public Subnet 2 ID"
     value = aws_subnet.public_subnet_2.cidr_block
 }
+
+output "vpc_id" {
+    description = "id of custome vpc"
+    value = aws_vpc.week_22_vpc.id
+}
